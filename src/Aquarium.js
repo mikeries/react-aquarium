adf08c4a665cadef367d14195a95a7a7e204700b
Michael Ries <michael.ries@gmail.com>
2017-10-08 10:45:20 -0500

    update styling, add clownfish

--- src/Aquarium.js
+++ src/Aquarium.js
@@ -4,19 +4,19 @@ import './style.css';
 
 class Aquarium extends Component {
   render() {
-    let waterStyle = {height: window.innerHeight, width: window.innerWidth}
     return (
       <div>
-        <div className='water' style={{...waterStyle, zIndex:-1}}></div>
-        <div className='water' style={{...waterStyle, zIndex:-10}}></div>
-        <div className='water' style={{...waterStyle, zIndex:-20}}></div>
-        <div className='water' style={{...waterStyle, zIndex:-30}}></div>
-        <div className='water' style={{...waterStyle, zIndex:-40}}></div>
-        <div className='water' style={{...waterStyle, zIndex:-50}}></div>
-        <div className='water' style={{...waterStyle, zIndex:-60}}></div>
+        <div className='water' style={{ zIndex:-1 }}></div>
+        <div className='water' style={{ zIndex:-10 }}></div>
+        <div className='water' style={{ zIndex:-20 }}></div>
+        <div className='water' style={{ zIndex:-30 }}></div>
+        <div className='water' style={{ zIndex:-40 }}></div>
+        <div className='water' style={{ zIndex:-50 }}></div>
+        <div className='water' style={{ zIndex:-60 }}></div>
         <Fish image='./images/tiny-small-pixel-fish-aquarium-animated-gif-picture-10.gif'/>
         <Fish image='./images/tiny-small-pixel-fish-aquarium-animated-gif-picture-19.gif'/>
         <Fish image='./images/tiny-small-pixel-fish-aquarium-animated-gif-picture-11.gif'/>
+        <Fish image='./images/clownfishb.gif'/>
       </div>
     );
   }
